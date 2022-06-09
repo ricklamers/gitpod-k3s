7c80441135425be82d9168af9b760055c63190e2
Rick Lamers <ricklamers@gmail.com>
2022-06-09 06:29:09 +0000

    Increase cores

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -8,7 +8,7 @@ outdir="${script_dirname}/_output"
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
 -boot c -m 16000M -hda "${outdir}/rootfs/hirsute-server-cloudimg-amd64.img" \
 -net user \
--smp 8 \
+-smp 16 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
 -nic user,hostfwd=tcp::2222-:22,hostfwd=tcp::6443-:6443 \
 -serial mon:stdio -display none
